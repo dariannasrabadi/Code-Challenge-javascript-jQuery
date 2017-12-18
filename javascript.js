94f6464916aad5e015976b6f61eed0a5640cbe64
Darian Nasrabadi <darian.nasrabadi@gmail.com>
2017-12-18 13:39:58 -0600

    create p element that denotes number of times generate was clicked

--- javascript.js
+++ javascript.js
@@ -1,14 +1,20 @@
 $(document).ready(onReady);
 
+var i = 0; // button click count
+
 function onReady() { //Start onReady Function
 $('#generator').on('click', divCreator);
-
 
 
 } // END: onReady
 
 function divCreator() {//start of divCreator
   console.log('I was clicked');
-  $('body').append('<div></div>');
+  i++; // adding count
+  var $div = $('<div></div>');
+  $div.append('<p>' + i + '</p>');
+
+
+  $('body').append($div);
 
 } // END: divCreator
