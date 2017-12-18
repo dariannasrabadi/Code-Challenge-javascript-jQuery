0cef84e0580ed1a56c3fc3b921781f72d7a829f3
Darian Nasrabadi <darian.nasrabadi@gmail.com>
2017-12-18 13:29:31 -0600

    button now add divs on click

--- javascript.js
+++ javascript.js
@@ -1,8 +1,14 @@
 $(document).ready(onReady);
 
 function onReady() { //Start onReady Function
-
+$('#generator').on('click', divCreator);
 
 
 
 } // END: onReady
+
+function divCreator() {//start of divCreator
+  console.log('I was clicked');
+  $('body').append('<div></div>');
+
+} // END: divCreator
